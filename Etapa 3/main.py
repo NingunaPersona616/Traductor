bfe15e93fe67ec4046ee57bab38e7b7e5684e400
Brandon Hernandez Ledezma <brandon.hernandez5150@alumnos.udg.mx>
2022-02-20 23:15:08 -0600

    Etapa 3 (Sintáctico usando Objetos)
    Analizador Sintáctico (Implementación usando Objetos)

---
+++ Etapa 3/main.py
@@ -0,0 +1,85 @@
+from Lexico import Lexico
+from Sintactico import Sintactico
+
+E=3
+d2=2
+d3=3
+d4=4
+r0=-1
+r1=-2
+r2=-3
+
+tablaLR1=[
+    [d2, 0, 0, 1],
+    [0, 0, r0, 0],
+    [0, d3, 0, 0],
+    [d4, 0, 0, 0],
+    [0, 0, r1, 0]
+]
+
+tablaLR2=[
+    [d2, 0, 0, 1],
+    [0, 0, r0, 0],
+    [0, d3, r2, 0],
+    [d2, 0, 0, 4],
+    [0, 0, r1, 0]
+]
+
+ej1_id=[3]#id de la regla de reducion del primer ejercicio
+ej1_regla=[3]#longitud de la regla de reducion del primer ejercicio
+
+ej2_id=[3,3]#id de las reglas de reducion del segundo ejercicio
+ej2_regla=[3, 1]#longitud de las reglas de reducion del segundo ejercicio
+
+
+Analizador=Lexico()
+
+
+#EJERCICO 1 ANALIZADOR SINTACTICO
+ejercicio1="hola+mundo"
+listaentrada1=list()
+print('\n\nEJERCICO 1 ANALIZADOR SINTACTICO\n\n')
+print("Proceso Lexico\n")
+while(Analizador.temp!="$"):
+    token=Analizador.lex(ejercicio1)
+
+    Analizador.Show_TokenType(token)
+    listaentrada1.append(token)
+
+print("\nProceso Sintactico\n")
+listaentrada1.reverse()
+
+syntax=Sintactico(tablaLR1, ej1_id, ej1_regla)
+
+sies=syntax.prueba(listaentrada1)
+
+if(sies):
+    print("\n", ejercicio1, "=Aceptacion")
+else:
+    print("\n", ejercicio1, "=Denegada")
+
+
+
+Analizador2=Lexico()
+#EJERCICO 2 ANALIZADOR SINTACTICO
+ejercicio2="a+b+c+d+e+f"
+listaentrada2=list()
+print('\n\nEJERCICO 2 ANALIZADOR SINTACTICO\n\n')
+print("Proceso Lexico\n")
+while(Analizador2.temp!="$"):
+    token=Analizador2.lex(ejercicio2)
+
+    Analizador2.Show_TokenType(token)
+    listaentrada2.append(token)
+
+print("\nProceso Sintactico\n")
+listaentrada2.reverse()
+
+syntax2=Sintactico(tablaLR2, ej2_id, ej2_regla)
+
+sies=syntax2.prueba(listaentrada2)
+
+if(sies):
+    print("\n", ejercicio2, "=Aceptacion")
+else:
+    print("\n", ejercicio2, "=Denegada")
--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Mini genarador lexico que el profesor dejó terminaramos, y que reconoce ids, '+
 Analizador sintactico que trabaja en conjunto con el analizador lexico para resolver los ejercicios 1 y 2 
 
 19/02/20222 No me di cuenta que las listas fuera del constructor se comparten con todas las instancias, aunque no genera ningun problema porque el constructor vuelve a pushear '$' y '0' y el algoritmo vuelve a funcionar como si fuera una pila vacia jajaja. Funciona correctamente simplemente es raro de ver.
+
+## Etapa 3
+Analizador Sintáctico (Implementación usando Objetos)
+
+IDEA: checar con más tablas LR
